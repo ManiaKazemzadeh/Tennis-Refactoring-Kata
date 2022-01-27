3ed9ce3974541d3163cd4a46d631ca25a9320c95
Mark Said Camilleri <mark.saidcamilleri@cazoo.co.uk>
2022-01-27 11:47:07 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- typescript/src/TennisGame1.ts
+++ typescript/src/TennisGame1.ts
@@ -1,57 +1,64 @@
-import { TennisGame } from './TennisGame';
+import {TennisGame} from './TennisGame';
 
 const scoreMap = {
-  0: "Love",
-  1: "Fifteen",
-  2: "Thirty",
-  3: "Forty"
+    0: "Love",
+    1: "Fifteen",
+    2: "Thirty",
+    3: "Forty"
 }
 
-const drawMap = {
-  0: "Love-All",
-  1: "Fifteen-All",
-  2: "Thirty-All",
-  3: "Deuce"
+const getDrawOutput = (score: number) => {
+    return {
+        0: "Love-All",
+        1: "Fifteen-All",
+        2: "Thirty-All",
+    }[score] || "Deuce";
+
 }
 
 export class TennisGame1 implements TennisGame {
-  private m_score1: number = 0;
-  private m_score2: number = 0;
-  private player1Name: string;
-  private player2Name: string;
+    private m_score1: number = 0;
+    private m_score2: number = 0;
+    private player1Name: string;
+    private player2Name: string;
 
-  constructor(player1Name: string, player2Name: string) {
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
-  }
+    constructor(player1Name: string, player2Name: string) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+    }
 
-  wonPoint(playerName: string): void {
-    if (playerName === 'player1')
-      this.m_score1 += 1;
-    else
-      this.m_score2 += 1;
-  }
+    wonPoint(playerName: string): void {
+        if (playerName === 'player1')
+            this.m_score1 += 1;
+        else
+            this.m_score2 += 1;
+    }
 
-  getScore(): string {
-    let score: string = '';
-    let tempScore: number = 0;
-    if (this.m_score1 === this.m_score2) {
-      this.m_score1 = drawMap[this.m_score1]
+    getScore(): string {
+        if (this.m_score1 === this.m_score2) {
+            return getDrawOutput(this.m_score1)
+        }
+
+        if (this.m_score1 >= 4 || this.m_score2 >= 4) {
+            const minusResult: number = this.m_score1 - this.m_score2;
+            if (minusResult === 1) return 'Advantage player1';
+            if (minusResult === -1) return 'Advantage player2';
+            if (minusResult >= 2) return 'Win for player1';
+            return 'Win for player2';
+        }
+
+        let score: string = '';
+        let tempScore: number = 0;
+
+        for (let i = 1; i < 3; i++) {
+            if (i === 1) tempScore = this.m_score1;
+            else {
+                score += '-';
+                tempScore = this.m_score2;
+            }
+            score += scoreMap[tempScore]
+
+        }
+        return score;
     }
-    else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-      const minusResult: number = this.m_score1 - this.m_score2;
-      if (minusResult === 1) score = 'Advantage player1';
-      else if (minusResult === -1) score = 'Advantage player2';
-      else if (minusResult >= 2) score = 'Win for player1';
-      else score = 'Win for player2';
-    }
-    else {
-      for (let i = 1; i < 3; i++) {
-        if (i === 1) tempScore = this.m_score1;
-        else { score += '-'; tempScore = this.m_score2; }
-        score += scoreMap[tempScore]
-      }
-    }
-    return score;
-  }
 }
